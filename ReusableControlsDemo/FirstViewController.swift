6eee4333b472daa8fe4856e0dcda07a946eb9252
Alejandro Rodriguez P <alejandrorodriguezpena@gmail.com>
2018-03-26 21:17:49 -0300

    Initial Commit

---
+++ ReusableControlsDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ReusableControlsDemo
+//
+//  Created by Alejandro  Rodriguez on 26-03-18.
+//  Copyright © 2018 Alejandro  Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ReusableControlsDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ReusableControlsDemo
+//
+//  Created by Alejandro  Rodriguez on 26-03-18.
+//  Copyright © 2018 Alejandro  Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
